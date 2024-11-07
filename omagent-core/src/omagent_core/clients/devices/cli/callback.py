1c64e30b3c9b59cb92486180373b4e98cd926b3c
潇陵 <347090563@qq.com>
2024-11-07 22:31:28 +0800

    fix input and callback bug
    fix input and callback bug

--- omagent-core/src/omagent_core/clients/devices/cli/callback.py
+++ omagent-core/src/omagent_core/clients/devices/cli/callback.py
@@ -8,83 +8,48 @@ from typing import Any, ClassVar
 
 from colorama import Fore, Style
 
+from omagent_core.clients.devices.app.schemas import InteractionType, MessageType
+from omagent_core.utils.registry import registry
 from omagent_core.utils.error import VQLError
 from omagent_core.utils.logger import logging
 import omagent_core.base
 from omagent_core.clients.base import CallbackBase
 
 
-
+@registry.register_component()
 class DefaultCallback(CallbackBase):
     bot_id: str = ""
-    start_time: int = time()
-    endpoint: str = ""
-    loop_count: ClassVar[dict] = defaultdict(int)
-    callback_count: ClassVar[dict] = defaultdict(int)
+    # start_time: int = time()
+    # endpoint: str = ""
+    # loop_count: ClassVar[dict] = defaultdict(int)
+    # callback_count: ClassVar[dict] = defaultdict(int)
 
-    def __init__(self, **data: Any):
-        super().__init__(**data)
-        self.endpoint = data.get("endpoint", "")
+    # def __init__(self, **data: Any):
+    #     super().__init__(**data)
+    #     self.endpoint = data.get("endpoint", "")
 
     def visualize_in_terminal(self, *args, **kwargs):
         pass
 
-    def info(self, msg):
+    def info(self, agent_id, progress, message):
         logging.info(
-            f"\n{Fore.BLUE}{json.dumps(msg, indent=2, ensure_ascii=False)}{Style.RESET_ALL}"
+            f"\n{Fore.BLUE}info:{agent_id} {progress} {message}{Style.RESET_ALL}"
         )
 
-    def send_block(self, msg):
-        caller, calling_chain = self.get_calling_class()
-        logging.info(
-            f"\n{Fore.BLUE}{json.dumps(msg, indent=2, ensure_ascii=False)}{Style.RESET_ALL}"
-        )
-        if len(calling_chain) > 0:
-            if len(calling_chain) == 1 and isinstance(
-                caller, omagent_core.engine.node.BaseLoop
-            ):
-                Path(
-                    f"{self.folder_name}/{'/'.join(calling_chain)}/{self.loop_count[calling_chain[-1]]}"
-                ).mkdir(parents=True, exist_ok=True)
-                self.loop_count[calling_chain[-1]] += 1
-            else:
-                for each in list(self.loop_count.keys()):
-                    calling_chain.insert(
-                        calling_chain.index(each) + 1, str(self.loop_count[each] - 1)
-                    )
-                if isinstance(caller, omagent_core.models.llms.base.BaseLLM):
-                    Path(f"{self.folder_name}/{'/'.join(calling_chain[:-1])}").mkdir(
-                        parents=True, exist_ok=True
-                    )
-                    json.dump(
-                        msg,
-                        open(
-                            f"{self.folder_name}/{'/'.join(calling_chain[:-1])}/{calling_chain[-1]}_{self.callback_count[calling_chain[-1]]}.json",
-                            "w",
-                        ),
-                        indent=2,
-                        ensure_ascii=False,
-                    )
-                else:
-                    Path(f"{self.folder_name}/{'/'.join(calling_chain)}").mkdir(
-                        parents=True, exist_ok=True
-                    )
-                    json.dump(
-                        msg,
-                        open(
-                            f"{self.folder_name}/{'/'.join(calling_chain)}/{self.callback_count[calling_chain[-1]]}.json",
-                            "w",
-                        ),
-                        indent=2,
-                        ensure_ascii=False,
-                    )
-                self.callback_count[calling_chain[-1]] += 1
+    def send_block(
+        self,
+        agent_id,
+        msg,
+        **kwargs
+    ):
+        logging.info(f"\n{Fore.BLUE}block:{msg}{Style.RESET_ALL}")
+        
 
-    def error(self, error: VQLError):
-        logging.error(f"\n{Fore.RED}{error}{Style.RESET_ALL}")
+    def error(self, agent_id, error_code, error_info, **kwargs):
+        logging.error(f"\n{Fore.RED}{error_info}{Style.RESET_ALL}")
 
-    def send_answer(self, msg):
-        raise NotImplementedError("Not implemented yet.")
+    def send_answer(self, agent_id, msg, **kwargs):
+        logging.info(f"\n{Fore.BLUE}answer:{msg}{Style.RESET_ALL}")
 
     def finish(self, **kwargs):
         def generate_tree(path, indent=""):
