89202a13aa9f02a4bc8efe8a98bbd1ff1093c7b1
潇陵 <347090563@qq.com>
2024-11-08 14:03:50 +0800

    Optimize the client module and remove unnecessary packages
    Optimize the client module and remove unnecessary packages

--- omagent-core/src/omagent_core/clients/devices/app/client.py
+++ omagent-core/src/omagent_core/clients/devices/app/client.py
@@ -1,11 +1,23 @@
-import os
+from omagent_core.utils.container import container
+from omagent_core.engine.configuration.configuration import Configuration
+container.register_connector(name='conductor_config', connector=Configuration)
+
 from omagent_core.engine.workflow.conductor_workflow import ConductorWorkflow
 from omagent_core.utils.build import build_from_file
-from omagent_core.utils.compile import compile
 from omagent_core.engine.automator.task_handler import TaskHandler
+from omagent_core.clients.devices.app.callback import AppCallback
+from omagent_core.clients.devices.app.input import AppInput
 import yaml
 from omagent_core.utils.container import container
 from omagent_core.utils.registry import registry
+from omagent_core.services.connectors.redis import RedisConnector
+
+registry.import_module()
+
+container.register_connector(name='redis_stream_client', connector=RedisConnector)
+container.register_stm(stm='RedisSTM')
+container.register_callback(callback=AppCallback)
+container.register_input(input=AppInput)
 
 
 class AppClient:
@@ -20,7 +32,7 @@ class AppClient:
         self._config_path = config_path
 
     def start_interactor(self):
-        worker_config = build_from_file(self._config_path + "/step1_simpleVQA/configs")
+        worker_config = build_from_file(self._config_path)
         self._task_handler_interactor = TaskHandler(worker_config=worker_config)
         self._task_handler_interactor.start_processes()
         workflow_execution_id = self._interactor.start_workflow_with_input(workflow_input={})
