e13879f55bcecb67986dfea1a0533e4936b64816
zhang_lu <zhang_lu@hzlh.com>
2024-11-08 21:53:24 +0800

    Fixed the issue where the botbase corresponding component did not update when the container component was updated.

--- omagent-core/src/omagent_core/utils/container.py
+++ omagent-core/src/omagent_core/utils/container.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Type
+from typing import Dict, List, Type, Optional
 from pydantic import BaseModel
 from omagent_core.utils.registry import registry
 
@@ -7,10 +7,10 @@ class Container:
     def __init__(self):
         self._connectors: Dict[str, BaseModel] = {}
         self._components: Dict[str, BaseModel] = {}
-        self._stm = None
-        self._ltm = None
-        self._callback = None
-        self._input = None
+        self._stm_name: Optional[str] = None
+        self._ltm: Optional[str] = None
+        self._callback: Optional[str] = None
+        self._input: Optional[str] = None
 
     def register_connector(
         self,
@@ -58,12 +58,12 @@ class Container:
             raise ValueError(f"Invalid component type: {type(component)}")
 
         if (name in self._components or component_name in self._components) and not overwrite:
-            return
+            return name or component_name
 
         required_connectors = self._get_required_connectors(component_cls)
         if required_connectors:
             for connector, cls_name in required_connectors:
-                if connector not in self._connectors:
+                if connector not in self._connectors.keys():
                     connector_cls = registry.get_connector(cls_name)
                     self.register_connector(connector_cls, connector)
                 config[connector] = self._connectors[connector]
@@ -93,35 +93,36 @@ class Container:
     
     def register_stm(self, stm: str|Type[BaseModel], name: str = None, config: dict = {}, overwrite: bool = False):
         name = self.register_component(stm, name, config, overwrite)
-        self._stm = self._components[name]
+        self._stm_name = name
 
     @property
     def stm(self) -> BaseModel:
-            return self._stm
+        return self.get_component(self._stm_name)
+
         
     def register_ltm(self, ltm: str|Type[BaseModel], name: str = None, config: dict = {}, overwrite: bool = False):
         name = self.register_component(ltm, name, config, overwrite)
-        self._ltm = self._components[name]
+        self._ltm_name = name
         
     @property
     def ltm(self) -> BaseModel:
-            return self._ltm
+        return self.get_component(self._ltm_name)
     
     def register_callback(self, callback: str|Type[BaseModel], name: str = None, config: dict = {}, overwrite: bool = False):
         name = self.register_component(callback, name, config, overwrite)
-        self._callback = self._components[name]
+        self._callback_name = name
         
     @property
     def callback(self) -> BaseModel:
-            return self._callback
+        return self.get_component(self._callback_name)
     
     def register_input(self, input: str|Type[BaseModel], name: str = None, config: dict = {}, overwrite: bool = False):
         name = self.register_component(input, name, config, overwrite)
-        self._input = self._components[name]
+        self._input_name = name
         
     @property
     def input(self) -> BaseModel:
-            return self._input
+        return self.get_component(self._input_name)
 
     def compile_config(self) -> None:
         config = {"connectors": {}, "components": {}}
@@ -134,7 +135,7 @@ class Container:
 
         return config
 
-    def update_from_config(self, config_data: dict) -> None:
+    def from_config(self, config_data: dict) -> None:
         """Update container from configuration
 
         Args:
@@ -161,7 +162,6 @@ class Container:
             for name, config in config_data["connectors"].items():
                 connector_cls = registry.get_connector(config.pop("name"))
                 if connector_cls:
-                    print(111, name, config)
                     self.register_connector(
                         name=name, connector=connector_cls, overwrite=True, **config
                     )
