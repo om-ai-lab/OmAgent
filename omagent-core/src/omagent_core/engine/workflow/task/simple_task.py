60d7486c74e10f2b71e2081e13e1beefe8d19d86
zhang_lu <zhang_lu@hzlh.com>
2024-11-26 00:13:05 +0800

    Support using class for task_def_name

--- omagent-core/src/omagent_core/engine/workflow/task/simple_task.py
+++ omagent-core/src/omagent_core/engine/workflow/task/simple_task.py
@@ -2,6 +2,7 @@ from typing_extensions import Self
 
 from omagent_core.engine.workflow.task.task import TaskInterface
 from omagent_core.engine.workflow.task.task_type import TaskType
+from omagent_core.engine.worker.base import BaseWorker
 
 
 class SimpleTask(TaskInterface):
@@ -13,7 +14,9 @@ class SimpleTask(TaskInterface):
         )
 
 
-def simple_task(task_def_name: str, task_reference_name: str, inputs: dict[str, object] = {}) -> TaskInterface:
+def simple_task(task_def_name: str|Type[BaseWorker], task_reference_name: str, inputs: dict[str, object] = {}) -> TaskInterface:
+    if issubclass(task_def_name, BaseWorker):
+        task_def_name = task_def_name.name
     task = SimpleTask(task_def_name=task_def_name, task_reference_name=task_reference_name)
     task.input_parameters.update(inputs)
     return task
